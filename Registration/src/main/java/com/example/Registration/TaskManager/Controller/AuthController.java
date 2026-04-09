68e0c4a90d67ba989cb13d0fc1c6b8c13de473a9
babisha04 <babishariya@gmail.com>
2026-04-09 23:03:16 +0530

    Added the AuthController which controls the RESTAPI for register and login.

---
+++ Registration/src/main/java/com/example/Registration/TaskManager/Controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.Registration.TaskManager.Controller;
+
+import com.example.Registration.TaskManager.Modal.User;
+import com.example.Registration.TaskManager.Service.AuthService;
+import com.example.Registration.TaskManager.dto.AuthResponse;
+import com.example.Registration.TaskManager.dto.LoginRequest;
+import com.example.Registration.TaskManager.dto.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth/")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest user){
+        String msg=authService.register(user);
+        return ResponseEntity.status(201).body(msg);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse>  login(@RequestBody LoginRequest log){
+        return ResponseEntity.status(201).body(authService.login(log));
+    }
+}
