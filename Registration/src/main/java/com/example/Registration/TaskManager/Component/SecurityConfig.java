6391d956beaae16cbadc35b66962ab6af1c9caae
babisha04 <babishariya@gmail.com>
2026-04-09 12:42:05 +0530

    Added the Security Config which controls who can access the API and how authentiation works.

---
+++ Registration/src/main/java/com/example/Registration/TaskManager/Component/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.Registration.TaskManager.Component;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password4j.BcryptPassword4jPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private  final  JwtAuthFilter jwtAuthFilter;
+    @Bean
+    public SecurityFilterChain securityFilterchain(HttpSecurity http) throws  Exception{
+        http.csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers("/api/auth/register","/api/auth/login").permitAll()
+                        .requestMatchers("/auth/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws  Exception{
+        return config.getAuthenticationManager();
+    }
+
+}
